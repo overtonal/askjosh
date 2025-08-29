67dc5232a7b9a24cc47fa4ba3cdb662132d6afab
overtonal <james.lambell@yahoo.com>
2025-08-29 20:06:26 +0100

    Updates

--- _app/immutable/chunks/1iUzh5yG.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_jomm3a?.base??"/askjosh",a=globalThis.__sveltekit_jomm3a?.assets??s;export{a,s as b};
---
+++ _app/immutable/chunks/D8jpzJvM.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_k0u20s?.base??"/askjosh",a=globalThis.__sveltekit_k0u20s?.assets??s;export{a,s as b};
